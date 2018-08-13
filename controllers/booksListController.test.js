2202e6fd28358d083885a06dc57c0a4b39c02a78
msevera <michael.svr@gmail.com>
2018-08-13 13:38:30 +0300

    Code refactoring

--- controllers/booksListController.test.js
+++ controllers/booksListController.test.js
@@ -27,33 +27,34 @@ beforeEach(() => {
 
 describe('testing BooksListController controller', () => {
   test('GetBook action success', async (done) => {
-    const id = 0;
+    const book = repository.book.getById(0);
     const sendFunc = (status, resource) => {
       expect(status).toBe(200);
-      expect(resource.id).toBe(id);
+      expect(resource.id).toBe(book._id);
       done();
     };
     const controller = new BookListController(
-        { params: { id }, send: sendFunc, repository, uriGenerator: new URIGenerator() },
+        { params: { id: book._id }, send: sendFunc, repository, uriGenerator: new URIGenerator() },
     );
     await controller.getBook();
   });
 
   test('RateBook action success', async (done) => {
-    const id = 0;
+    const book = repository.book.getById(0);
     const sendFunc = (status, resource) => {
       expect(status).toBe(200);
-      expect(resource.id).toBe(id);
+      expect(resource.id).toBe(book._id);
       expect(resource.rating).toBe(5);
       done();
     };
     const controller = new BookListController(
-        { params: { id }, body: { rating: 5 }, send: sendFunc, repository, uriGenerator: new URIGenerator() },
+        { params: { id: book._id }, body: { rating: 5 }, send: sendFunc, repository, uriGenerator: new URIGenerator() },
     );
     await controller.rateBook();
   });
 
   test('RemoveBook action success', async (done) => {
+    const book = repository.book.getById(0);
     const allBooksLength = repository.book.getAllBooks().length;
     const expectedResult = allBooksLength - 1;
     const sendFunc = (status) => {
@@ -61,7 +62,7 @@ describe('testing BooksListController controller', () => {
       expect(repository.book.getAllBooks().length).toBe(expectedResult);
       done();
     };
-    const controller = new BookListController({ send: sendFunc, repository, uriGenerator: new URIGenerator() });
+    const controller = new BookListController({ params: { id: book._id }, send: sendFunc, repository, uriGenerator: new URIGenerator() });
     await controller.removeBook();
   });
 });
